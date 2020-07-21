4bade5991edf6ae3d20f93673a57d5411e2552ee
Cindy Billick <cbillick28@gmail.com>
2020-07-21 09:35:41 -0400

    Add SQL Challenge File

---
+++ SQL Challenges.sql
@@ -0,0 +1,26 @@
+-- Find all products
+SELECT * FROM products;
+-- Find all products that cost $1400
+SELECT * FROM products WHERE Price=1400;
+-- Find all products that cost 11.99 or 13.99
+SELECT * FROM products WHERE Price = 11.99 or Price = 13.99; 
+-- Find all prodcuts that do NOT cost 11.99 using NOT
+SELECT * FROM products WHERE NOT Price= 11.99;
+-- Find all prodcuts and sort them by price from greatest to least (decending order)
+SELECT * FROM products ORDEr BY Price DESC;
+-- Find all employees who do not have a middle initial
+SELECT LastName,FirstName FROM employees Where MiddleInitial IS NULL; 
+-- Find Distinct product prices
+SELECT DISTINCT Price FROM products;
+-- Find all employees whose first name starts with the letter 'j'
+  SELECT * FROM employees WHERE FirstName LIKE 'j%';
+-- Find all Macbooks
+SELECT * FROM products WHERE Name = 'Macbook';   
+-- Find all prodcuts that are on sale
+SELECT * FROM products WHERE OnSale=1;
+-- Find the average price of all prodcuts
+SELECT AVG (Price) FROM products;
+-- Find all Geek Squad employees who do not have a middle initial
+SELECT LastName, FirstName, Title, MiddleInitial FROM employees WHERE MiddleInitial IS NULL AND Title = 'Geek Squad';
+-- Find all products from the products table whose stock level is in the arrange of 500 to 1200.  Order by Price from least to greatest
+SELECT * FROM products WHERE StockLevel BETWEEN 500 and 1200 ORDER BY Price ASC;
